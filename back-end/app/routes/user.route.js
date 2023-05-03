69f4a447e563d782d3a8ab2ec441f0d2b4ee0afa
tienb1910001 <tienb1910001@student.ctu.edu.vn>
2023-05-03 14:55:26 +0700

    cai dat routes

---
+++ back-end/app/routes/user.route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const users = require("../controllers/user.controller");
+
+const router = express.Router();
+
+router.route("/")
+    .get(users.findAll)
+    .post(users.create)
+    .delete(users.deleteAll);
+
+router.route("/:id")
+    .get(users.findOne)
+    .put(users.update)
+    .delete(users.delete);
+
+module.exports = router;
